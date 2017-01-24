e2cf8df355334f03a54c2796e7e011fe6c7eded6
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-01-25 01:41:31 +0500

    Добавлен скрипт для вывода статистики

--- js/stat.js
+++ js/stat.js
@@ -1,60 +1,100 @@
 'use strict';
 
-function isUser (name) {
-  return name == 'Вы';
-}
+// Отрисовать поле-облачко и текст поздравления
 
-function randomBlue () {
-  return Math.floor(50 + Math.random() * (255 + 1 - 50));
-}
-
-window.renderStatistics = function (ctx, names, times) {
-  var widthCloud = 420;
-  var heightCloud = 270;
-
+var drawCloud = function (ctx, cloudX, cloudY, widthCloud, heightCloud) {
+  ctx.beginPath();
+  ctx.moveTo(cloudX + 10, cloudY + 10);
+  ctx.lineTo(cloudX + widthCloud + 10, cloudY + 10);
+  ctx.lineTo(cloudX + widthCloud - 40 + 10, cloudY + heightCloud + 10);
+  ctx.lineTo(cloudX - 40 + 10, cloudY + heightCloud + 10);
+  ctx.closePath();
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, widthCloud, heightCloud);
+  ctx.stroke();
+  ctx.fill();
 
+  ctx.beginPath();
+  ctx.moveTo(cloudX, cloudY);
+  ctx.lineTo(cloudX + widthCloud, cloudY);
+  ctx.lineTo(cloudX + widthCloud - 40, cloudY + heightCloud);
+  ctx.lineTo(cloudX - 40, cloudY + heightCloud);
+  ctx.closePath();
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.strokeRect(100, 10, widthCloud, heightCloud);
-  ctx.fillRect(100, 10, widthCloud, heightCloud);
+  ctx.stroke();
+  ctx.fill();
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-  ctx.fillText('Ура вы победили!', 110, 30);
-  ctx.fillText('Список результатов:', 110, 50);
-
-  var maxTime = 0;
-  for (var i = 0; i < times.length; i++) {
-    maxTime = (maxTime < times[i]) ? times[i] : maxTime;
-  }
-
-  var columnMaxHeight = 150;
-  var columnWidth = 40;
-  var columnHeight;
-  var columnDistance = 50;
-  var columnStartX = 130;
-  var columnStartY = 250;
-
-  for (i = 0; i < names.length; i++) {
-    if (isUser(names[i])) {
-      ctx.fillStyle = 'rgb(255, 0, 0)';
-    } else {
-      ctx.fillStyle = 'rgb(0,0,' + randomBlue() + ')';
-    }
-    columnHeight = -(columnMaxHeight / maxTime * times[i]);
-    ctx.fillRect(columnStartX, columnStartY, columnWidth, columnHeight);
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(Math.round(times[i]), columnStartX, columnHeight + 240);
-    ctx.fillText(names[i], columnStartX, columnStartY + 20);
-    columnStartX += columnWidth + columnDistance;
-  }
-
+  ctx.fillText('Ура вы победили!', cloudX + 50, cloudY + 20);
+  ctx.fillText('Список результатов:', cloudX + 50, cloudY + 40);
 };
 
-// var canvas = document.querySelector('canvas');
-// var ctx = canvas.getContext('2d');
-// var names = ['Борис', 'Вы', 'Карл', 'Кекс'];
-// var times = [30.1, 55.99, 100, 26.63];
-// window.renderStatistics(ctx, names, times);
+/* Сортировать массив времени по возрастанию, а также менять местами элементы
+массива имен, для сохранения связи между парами "время - имя игрока" */
+
+var sortArrays = function (times, names) {
+  var cache;
+  for (var i = 0; i < times.length - 1; i++) {
+    for (var j = 0; j < times.length - 1 - i; j++) {
+      if (times[j + 1] < times[j]) {
+        cache = times[j + 1];
+        times[j + 1] = times[j];
+        times[j] = cache;
+
+        cache = names[j + 1];
+        names[j + 1] = names[j];
+        names[j] = cache;
+      }
+    }
+  }
+};
+
+var isUser = function (name) {
+  return name === 'Вы';
+};
+
+// Вывести детали статистики: имя игрока + время прохождения
+
+var showDetail = function (ctx, name, time, columnStartX, columnStartY, columnWidth, columnHeight) {
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.fillText(Math.round(time), columnStartX, columnHeight + 240);
+  ctx.fillText(name, columnStartX, columnStartY + 20);
+};
+
+// Покрасить колонки в синий цвет в зависимости от значения времени
+var paintBlue = function (time, maxTime) {
+  return 'rgb(0, 0, ' + Math.round(255 / maxTime * time) + ')';
+};
+
+// Отрисовать столбцы гистограммы
+var drawColumns = function (ctx, names, times, columnStartX, columnStartY, columnWidth, columnMaxHeight, columnDistance) {
+  var maxTime = times[times.length - 1];
+  var columnHeight;
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = (isUser(names[i])) ? 'rgb(255, 0, 0)' : paintBlue(times[i], maxTime);
+    columnHeight = -(columnMaxHeight / maxTime * times[i]);
+    ctx.fillRect(columnStartX, columnStartY, columnWidth, columnHeight);
+    showDetail(ctx, names[i], times[i], columnStartX, columnStartY, columnWidth, columnHeight, columnDistance);
+    columnStartX += columnWidth + columnDistance;
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  var cloudStartX = 100;
+  var cloudStartY = 10;
+  var widthCloud = 420;
+  var heightCloud = 270;
+
+  drawCloud(ctx, cloudStartX, cloudStartY, widthCloud, heightCloud);
+  sortArrays(times, names);
+
+  var columnStartX = 130;
+  var columnStartY = 250;
+  var columnWidth = 40;
+  var columnMaxHeight = 150;
+  var columnDistance = 50;
+
+  drawColumns(ctx, names, times, columnStartX, columnStartY, columnWidth, columnMaxHeight, columnDistance);
+};
