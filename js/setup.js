19f928c92660f887cf9ea4e08785c1c8cc61538d
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-02-11 22:29:06 +0500

    Инкапсуляция

--- js/setup.js
+++ js/setup.js
@@ -1,81 +1,83 @@
 'use strict';
+(function () {
+  var setupOpen = document.querySelector('.setup-open');
+  var setupOpenBtn = setupOpen.querySelector('.setup-open-icon');
+  var setupWindow = document.querySelector('.setup');
+  var setupClose = setupWindow.querySelector('.setup-close');
+  var fieldUserName = setupWindow.querySelector('.setup-user-name');
+  var btnSubmit = setupWindow.querySelector('.setup-submit');
+  var wizardCoat = setupWindow.querySelector('#wizard-coat');
+  var wizardCoatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var wizardEyes = setupWindow.querySelector('#wizard-eyes');
+  var wizardEyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+  var wizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
+  var wizardFireballColors = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
 
-var setupOpen = document.querySelector('.setup-open');
-var setupOpenBtn = setupOpen.querySelector('.setup-open-icon');
-var setupWindow = document.querySelector('.setup');
-var setupClose = setupWindow.querySelector('.setup-close');
-var fieldUserName = setupWindow.querySelector('.setup-user-name');
-var btnSubmit = setupWindow.querySelector('.setup-submit');
-var wizardCoat = setupWindow.querySelector('#wizard-coat');
-var wizardCoatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var wizardEyes = setupWindow.querySelector('#wizard-eyes');
-var wizardEyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
-var wizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
-var wizardFireballColors = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
+  fieldUserName.required = true;
+  fieldUserName.maxLength = 50;
 
-fieldUserName.required = true;
-fieldUserName.maxLength = 50;
+  var showDialog = function () {
+    document.addEventListener('keydown', escapePressHandler);
+    setupWindow.classList.remove('invisible');
+    setupOpenBtn.setAttribute('aria-pressed', true);
+    setupClose.setAttribute('aria-pressed', false);
+  };
 
-var showDialog = function () {
-  document.addEventListener('keydown', escapePressHandler);
-  setupWindow.classList.remove('invisible');
-  setupOpenBtn.setAttribute('aria-pressed', true);
-  setupClose.setAttribute('aria-pressed', false);
-};
+  var hideDialog = function () {
+    setupWindow.classList.add('invisible');
+    setupOpenBtn.setAttribute('aria-pressed', false);
+    setupClose.setAttribute('aria-pressed', true);
+    document.removeEventListener('keydown', escapePressHandler);
+  };
 
-var hideDialog = function () {
-  setupWindow.classList.add('invisible');
-  setupOpenBtn.setAttribute('aria-pressed', false);
-  setupClose.setAttribute('aria-pressed', true);
-  document.removeEventListener('keydown', escapePressHandler);
-};
+  var escapePressHandler = function (evt) {
+    if (window.checkEvents.checkPressedEsc(evt)) {
+      hideDialog();
+    }
+  };
 
-var escapePressHandler = function (evt) {
-  if (window.checkPressedEsc(evt)) {
-    hideDialog();
-  }
-};
+  setupOpen.addEventListener('click', showDialog);
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (window.checkEvents.checkPressedEnter(evt)) {
+      showDialog();
+    }
+  });
 
-setupOpen.addEventListener('click', showDialog);
-setupOpen.addEventListener('keydown', function (evt) {
-  if (window.checkPressedEnter(evt)) {
-    showDialog();
-  }
-});
+  setupClose.addEventListener('click', hideDialog);
+  setupClose.addEventListener('keydown', function (evt) {
+    if (window.checkEvents.checkPressedEnter(evt)) {
+      hideDialog();
+    }
+  });
 
-setupClose.addEventListener('click', hideDialog);
-setupClose.addEventListener('keydown', function (evt) {
-  if (window.checkPressedEnter(evt)) {
-    hideDialog();
-  }
-});
+  btnSubmit.addEventListener('click', hideDialog);
+  btnSubmit.addEventListener('keydown', function (evt) {
+    if (window.checkEvents.checkPressedEnter(evt)) {
+      hideDialog();
+    }
+  });
 
-btnSubmit.addEventListener('click', hideDialog);
-btnSubmit.addEventListener('keydown', function (evt) {
-  if (window.checkPressedEnter(evt)) {
-    hideDialog();
-  }
-});
+  window.colorizeElement(wizardCoat, wizardCoatColors, 'fill');
+  window.colorizeElement(wizardEyes, wizardEyesColors, 'fill');
+  window.colorizeElement(wizardFireball, wizardFireballColors, 'backgroundColor');
 
-window.colorizeElement(wizardCoat, wizardCoatColors, 'fill');
-window.colorizeElement(wizardEyes, wizardEyesColors, 'fill');
-window.colorizeElement(wizardFireball, wizardFireballColors, 'backgroundColor');
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,16 @@
 'use strict';
 
-window.utils = {};
-
-window.utils.getRandomElement = function (array) {
-  return array[Math.floor(Math.random() * array.length)];
-};
-
-window.utils.getRandomElementExcept = function (array, element) {
-  var currentElement = element;
-  while (currentElement === element) {
-    currentElement = window.utils.getRandomElement(array);
-  }
-  return currentElement;
-};
+window.utils = (function () {
+  return {
+    getRandomElement: function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+    },
+    getRandomElementExcept: function (array, element) {
+      var currentElement = element;
+      while (currentElement === element) {
+        currentElement = window.utils.getRandomElement(array);
+      }
+      return currentElement;
+    }
+  };
+})();
