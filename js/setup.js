5beb70e0fd2670b4e7c0113cad3ef39a97e38ccd
Feofilov Ilya <feofilov.ilya@gmail.com>
2017-02-13 19:58:09 +0500

    Помогите

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,5 @@
 'use strict';
+
 (function () {
   var setupOpen = document.querySelector('.setup-open');
   var setupOpenBtn = setupOpen.querySelector('.setup-open-icon');
@@ -35,81 +36,87 @@
   fieldUserName.required = true;
   fieldUserName.maxLength = 50;
 
-  var onSetupOpen = null;
+  var enableSetup = function () {
+    var onSetupOpen = null;
 
-  var showDialog = function (callback) {
-    document.addEventListener('keydown', escapePressHandler);
-    setupWindow.classList.remove('invisible');
-    setupOpenBtn.setAttribute('aria-pressed', true);
-    setupClose.setAttribute('aria-pressed', false);
-    if (typeof callback === 'function') {
+    var hideDialog = function () {
+      setupWindow.classList.add('invisible');
+      setupOpenBtn.setAttribute('aria-pressed', false);
+      setupClose.setAttribute('aria-pressed', true);
+      document.removeEventListener('keydown', escapePressHandler);
+      setupClose.removeEventListener('click', hideDialog);
+      setupClose.removeEventListener('keydown', offSetupKeydown);
+      btnSubmit.removeEventListener('click', hideDialog);
+      btnSubmit.removeEventListener('keydown', offSetupKeydown);
+
+      if (typeof onSetupOpen === 'function') {
+        onSetupOpen();
+      }
+    };
+
+    var escapePressHandler = function (evt) {
+      if (window.checkEvents.checkPressedEsc(evt)) {
+        hideDialog();
+      }
+    };
+
+    var offSetupKeydown = function (evt) {
+      if (window.checkEvents.checkPressedEnter(evt)) {
+        hideDialog();
+      }
+    };
+
+    var showDialog = function () {
+      document.addEventListener('keydown', escapePressHandler);
+      setupWindow.classList.remove('invisible');
+      setupOpenBtn.setAttribute('aria-pressed', true);
+      setupClose.setAttribute('aria-pressed', false);
+    };
+
+    setupClose.addEventListener('click', hideDialog);
+    setupClose.addEventListener('keydown', offSetupKeydown);
+
+    btnSubmit.addEventListener('click', hideDialog);
+    btnSubmit.addEventListener('keydown', offSetupKeydown);
+
+    var colorizingType = function (element, colors, property) {
+      var changeColor = function () {
+        currentValue = window.utils.getRandomElementExcept(colors, currentValue);
+        element.style[property] = currentValue;
+      };
+      var currentValue = element.style[property];
+      element.addEventListener('click', changeColor);
+      element.addEventListener('keydown', function (evt) {
+        if (window.checkEvents.checkPressedEnter(evt)) {
+          changeColor();
+        }
+      });
+    };
+
+    window.colorizeElement(function () {
+      colorizingType(wizardCoat, wizardCoatColors, 'fill');
+    });
+    window.colorizeElement(function () {
+      colorizingType(wizardEyes, wizardEyesColors, 'fill');
+    });
+    window.colorizeElement(function () {
+      colorizingType(wizardFireball, wizardFireballColors, 'backgroundColor');
+    });
+
+    return function (callback) {
+      showDialog();
       onSetupOpen = callback;
-    }
-  };
-
-  var hideDialog = function () {
-    setupWindow.classList.add('invisible');
-    setupOpenBtn.setAttribute('aria-pressed', false);
-    setupClose.setAttribute('aria-pressed', true);
-    document.removeEventListener('keydown', escapePressHandler);
-    if (typeof onSetupOpen === 'function') {
-      onSetupOpen();
-    }
-  };
-
-  var escapePressHandler = function (evt) {
-    if (window.checkEvents.checkPressedEsc(evt)) {
-      hideDialog();
-    }
+      window.console.log(onSetupOpen);
+    }();
   };
 
   var onSetupKeydown = function (evt) {
     if (window.checkEvents.checkPressedEnter(evt)) {
-      showDialog(function () {
+      enableSetup(function () {
         setupOpenBtn.focus();
       });
     }
   };
-
-  setupOpen.addEventListener('click', showDialog);
+  setupOpen.addEventListener('click', enableSetup);
   setupOpen.addEventListener('keydown', onSetupKeydown);
-
-  setupClose.addEventListener('click', hideDialog);
-  setupClose.addEventListener('keydown', function (evt) {
-    if (window.checkEvents.checkPressedEnter(evt)) {
-      hideDialog();
-    }
-  });
-
-  btnSubmit.addEventListener('click', hideDialog);
-  btnSubmit.addEventListener('keydown', function (evt) {
-    if (window.checkEvents.checkPressedEnter(evt)) {
-      hideDialog();
-    }
-  });
-
-  var colorizingType = function (element, colors, property) {
-    var changeColor = function () {
-      currentValue = window.utils.getRandomElementExcept(colors, currentValue);
-      element.style[property] = currentValue;
-    };
-    var currentValue = element.style[property];
-    element.addEventListener('click', changeColor);
-    element.addEventListener('keydown', function (evt) {
-      if (window.checkEvents.checkPressedEnter(evt)) {
-        changeColor();
-      }
-    });
-  };
-
-  window.colorizeElement(function () {
-    colorizingType(wizardCoat, wizardCoatColors, 'fill');
-  });
-  window.colorizeElement(function () {
-    colorizingType(wizardEyes, wizardEyesColors, 'fill');
-  });
-  window.colorizeElement(function () {
-    colorizingType(wizardFireball, wizardFireballColors, 'backgroundColor');
-  });
-
 })();
